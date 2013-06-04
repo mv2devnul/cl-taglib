5c022c775f38db9cc1ec44136bcc255b2ed6700d
Mark VandenBrink <mark@vandenbrink.us>
2013-06-04 16:04:59 -0500

    Minor tweaks. 	  - removed 'use-package' 	  - reworked the error handling slightly.

--- examples.lisp
+++ examples.lisp
@@ -1,5 +1,4 @@
 (in-package :cl-user)
-(use-package :ltag)
 
 ;;; test calls...
 (defmacro redirect-output (filename &rest body)
@@ -9,18 +8,25 @@
 		 ,@body)
 	   ,@body))
 	   
-
 (defun read-tag-file (fn)
   "read & print all simple tag/audio data from a file"
-  (handler-case (with-open-audio-file (f fn)
-				  (format t "File: ~a~%" (af-path f))
+  (handler-case (ltag:with-open-audio-file (f fn)
+				  (format t "got here~%")
+				  (format t "File: ~a~%" (ltag:af-path f))
 				  (format t "~tTitle: ~a~%~tArtist: ~a~%~tAlbum: ~a~%~tComment: ~a~%~tGenre: ~a~%~tYear: ~a~%~tTrack: ~a~%~tType: ~a~%"
-						  (af-title f) (af-artist f) (af-album f) (af-comment f) (af-genre f) (af-year f) (af-track f) (af-type f))
+						  (ltag:af-title f) (ltag:af-artist f) (ltag:af-album f) (ltag:af-comment f) (ltag:af-genre f)
+						  (ltag:af-year f) (ltag:af-track f) (ltag:af-type f))
 				  (format t "~tLength: ~a~%~tBitrate: ~a~%~tSamplerate: ~a~%~tChannels: ~a~%"
-										 (af-length f) (af-bitrate f) (af-samplerate f) (af-channels f)))
-	(bad-file (c) (declare (ignore c))
-	  (format t "File <~a> not recognized as an audio file~%" fn))))
+										 (ltag:af-length f) (ltag:af-bitrate f) (ltag:af-samplerate f) (ltag:af-channels f)))
+	(ltag:bad-file (c)
+	  (format t "Problem opening ~a: ~a~%" fn c))
+	(condition (c)
+	  (format t "Unknown problem with ~a: ~a~%" fn c))))
 
 (defun read-tags-from-dir (dir &key (output nil))
   "walk a diretory and read all audio files, printing all tag/audio data"
-  (redirect-output output (osicat:walk-directory dir #'read-tag-file)))
+  (let ((count 0))
+	(redirect-output output (progn 
+							  (osicat:walk-directory dir (lambda (file) (incf count) (read-tag-file file)))
+							  (format t "Processed ~,d files~%" count)))))
+	))
